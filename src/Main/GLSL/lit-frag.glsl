b4357f3cf3b5ce32db5c0b1131a1c7ff6f9d176e
Eric Mellino <erme@microsoft.com>
2016-06-22 19:40:38 -0700

    Implement color tinting in GLSL shader.

--- src/Main/GLSL/lit-frag.glsl
+++ src/Main/GLSL/lit-frag.glsl
@@ -6,6 +6,12 @@ uniform LightBuffer
     vec3 lightDirection;
 };
 
+uniform TintInfoBuffer
+{
+   vec3 tintColor;
+   float tintFactor;
+};
+
 uniform sampler2D surfaceTexture;
 
 in vec3 normal;
@@ -24,6 +30,7 @@ void main()
     float effectiveness = dot(normal, lightDir);
     float lightEffectiveness = clamp(effectiveness, 0, 1);
     vec4 lightColor = clamp(diffuseColor * lightEffectiveness, 0, 1);
-    outputColor = clamp((lightColor * color) + (ambientColor * color), 0, 1);
+    vec4 litColor = clamp((lightColor * color) + (ambientColor * color), 0, 1);
+    outputColor = (litColor * (1 - tintFactor) + (tintFactor * vec4(tintColor, 1)));
 
 }
