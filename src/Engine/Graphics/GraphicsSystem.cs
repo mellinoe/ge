8f54a9cd399490a226538f321bdddf2ae307c08c
Eric Mellino <erme@microsoft.com>
2016-08-16 00:43:35 -0700

    Clean up GraphicsSystem

--- src/Engine/Graphics/GraphicsSystem.cs
+++ src/Engine/Graphics/GraphicsSystem.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using Veldrid;
 using Veldrid.Graphics;
@@ -12,34 +14,20 @@ namespace Engine.Graphics
 {
     public class GraphicsSystem : GameSystem
     {
-        private readonly OctreeVisibilityManager _visiblityManager = new OctreeVisibilityManager();
-
-        private readonly Renderer _renderer;
-        private readonly PipelineStage[] _pipelineStages;
-
-        internal void AddPointLight(PointLight pointLight)
-        {
-            _pointLights.Add(pointLight);
-            if (_pointLights.Count > PointLightsBuffer.MaxLights)
-            {
-                Console.WriteLine($"Only {PointLightsBuffer.MaxLights} point lights are supported. PointLight {pointLight} will not be active.");
-            }
-        }
-
-        internal void RemovePointLight(PointLight pointLight)
-        {
-            if (!_pointLights.Remove(pointLight))
-            {
-                throw new InvalidOperationException($"Couldn't remove point light {pointLight}, it wasn't added.");
-            }
-        }
+        private static readonly DynamicDataProvider<LightInfo> _noLightProvider
+            = new DynamicDataProvider<LightInfo>(new LightInfo(RgbaFloat.Black, Vector3.Zero));
 
         private readonly Window _window;
+        private readonly Renderer _renderer;
+        private readonly PipelineStage[] _pipelineStages;
+        private readonly OctreeVisibilityManager _visiblityManager = new OctreeVisibilityManager();
         private readonly Dictionary<BoundsRenderItem, BoundsRenderItemEntry> _boundsRenderItemEntries = new Dictionary<BoundsRenderItem, BoundsRenderItemEntry>();
+        private readonly DynamicDataProvider<PointLightsBuffer> _pointLightsProvider = new DynamicDataProvider<PointLightsBuffer>();
+        private readonly List<PointLight> _pointLights = new List<PointLight>();
 
-        private OctreeRenderer<RenderItem> _octreeRenderer;
         private BoundingFrustum _frustum;
         private Camera _mainCamera;
+        private OctreeRenderer<RenderItem> _octreeRenderer;
 
         public ShadowMapStage ShadowMapStage { get; }
 
@@ -49,33 +37,13 @@ namespace Engine.Graphics
 
         public Camera MainCamera => _mainCamera;
 
-        private DynamicDataProvider<PointLightsBuffer> _pointLightsProvider = new DynamicDataProvider<PointLightsBuffer>();
-        private readonly List<PointLight> _pointLights = new List<PointLight>();
-
-        public void SetViewFrustum(ref BoundingFrustum frustum)
-        {
-            _frustum = frustum;
-            ((StandardPipelineStage)_pipelineStages[1]).CameraFrustum = frustum;
-            ((StandardPipelineStage)_pipelineStages[2]).CameraFrustum = frustum;
-        }
-
-        public void SetMainCamera(Camera camera)
-        {
-            _mainCamera = camera;
-            ShadowMapStage.MainCamera = camera;
-
-            Context.RegisterGlobalDataProvider("ViewMatrix", _mainCamera.ViewProvider);
-            Context.RegisterGlobalDataProvider("ProjectionMatrix", _mainCamera.ProjectionProvider);
-            Context.RegisterGlobalDataProvider("CameraInfo", _mainCamera.CameraInfoProvider);
-        }
-
-        public void SetDirectionalLight(DirectionalLight directionalLight)
-        {
-            ShadowMapStage.Light = directionalLight;
-        }
-
         public GraphicsSystem(OpenTKWindow window)
         {
+            if (window == null)
+            {
+                throw new ArgumentNullException(nameof(window));
+            }
+
             _window = window;
             Context = CreatePlatformDefaultContext(window);
             MaterialCache = new MaterialCache(Context.ResourceFactory);
@@ -92,30 +60,111 @@ namespace Engine.Graphics
             Context.RegisterGlobalDataProvider("PointLights", _pointLightsProvider);
         }
 
-        private static RenderContext CreatePlatformDefaultContext(OpenTKWindow window)
+        public void SetViewFrustum(ref BoundingFrustum frustum)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            _frustum = frustum;
+            ((StandardPipelineStage)_pipelineStages[1]).CameraFrustum = frustum;
+            ((StandardPipelineStage)_pipelineStages[2]).CameraFrustum = frustum;
+        }
+
+        public void SetMainCamera(Camera camera)
+        {
+            if (camera == null)
             {
-                return new D3DRenderContext(window);
+                throw new ArgumentNullException(nameof(camera));
             }
-            else
+
+            _mainCamera = camera;
+            ShadowMapStage.MainCamera = camera;
+
+            Context.RegisterGlobalDataProvider("ViewMatrix", _mainCamera.ViewProvider);
+            Context.RegisterGlobalDataProvider("ProjectionMatrix", _mainCamera.ProjectionProvider);
+            Context.RegisterGlobalDataProvider("CameraInfo", _mainCamera.CameraInfoProvider);
+        }
+
+        public void SetDirectionalLight(DirectionalLight directionalLight)
+        {
+            if (directionalLight == null)
             {
-                return new OpenGLRenderContext(window, false);
+                throw new ArgumentNullException(nameof(directionalLight));
+            }
+
+            ShadowMapStage.Light = directionalLight;
+            Context.RegisterGlobalDataProvider("LightBuffer", directionalLight.LightProvider);
+        }
+
+        public void UnsetDirectionalLight(DirectionalLight directionalLight)
+        {
+            if (directionalLight == null)
+            {
+                throw new ArgumentNullException(nameof(directionalLight));
+            }
+
+            if (ShadowMapStage.Light == directionalLight)
+            {
+                ShadowMapStage.Light = null;
+                Context.RegisterGlobalDataProvider("LightBuffer", _noLightProvider);
+            }
+        }
+
+        public void AddPointLight(PointLight pointLight)
+        {
+            if (pointLight == null)
+            {
+                throw new ArgumentNullException(nameof(pointLight));
+            }
+
+            _pointLights.Add(pointLight);
+            if (_pointLights.Count > PointLightsBuffer.MaxLights)
+            {
+                Console.WriteLine($"Only {PointLightsBuffer.MaxLights} point lights are supported. PointLight {pointLight} will not be active.");
+            }
+        }
+
+        public void RemovePointLight(PointLight pointLight)
+        {
+            if (pointLight == null)
+            {
+                throw new ArgumentNullException(nameof(pointLight));
+            }
+
+            if (!_pointLights.Remove(pointLight))
+            {
+                throw new InvalidOperationException($"Couldn't remove point light {pointLight}, it wasn't added.");
             }
         }
 
         public void AddFreeRenderItem(RenderItem ri)
         {
+            if (ri == null)
+            {
+                throw new ArgumentNullException(nameof(ri));
+            }
+
             _visiblityManager.AddRenderItem(ri);
         }
 
         public void RemoveFreeRenderItem(RenderItem ri)
         {
+            if (ri == null)
+            {
+                throw new ArgumentNullException(nameof(ri));
+            }
+
             _visiblityManager.RemoveRenderItem(ri);
         }
 
         public void AddRenderItem(BoundsRenderItem bri, Transform transform)
         {
+            if (bri == null)
+            {
+                throw new ArgumentNullException(nameof(bri));
+            }
+            if (transform == null)
+            {
+                throw new ArgumentNullException(nameof(transform));
+            }
+
             var octreeItem = _visiblityManager.AddRenderItem(bri.Bounds, bri);
             BoundsRenderItemEntry brie = new BoundsRenderItemEntry(bri, transform, octreeItem);
             _boundsRenderItemEntries.Add(bri, brie);
@@ -124,6 +173,11 @@ namespace Engine.Graphics
 
         public void RemoveRenderItem(BoundsRenderItem bri)
         {
+            if (bri == null)
+            {
+                throw new ArgumentNullException(nameof(bri));
+            }
+
             BoundsRenderItemEntry brie;
             if (!_boundsRenderItemEntries.TryGetValue(bri, out brie))
             {
@@ -134,31 +188,6 @@ namespace Engine.Graphics
             brie.Transform.TransformChanged -= brie.OnTransformChanged;
 
             _boundsRenderItemEntries.Remove(bri);
-        }
-
-        protected override void UpdateCore(float deltaSeconds)
-        {
-            float r = 0.8f;
-            float g = 0.8f;
-            float b = 0.8f;
-            Context.ClearColor = new RgbaFloat(r, g, b, 1.0f);
-
-            UpdatePointLightBuffer();
-
-            _visiblityManager.Octree.ApplyPendingMoves();
-            _renderer.RenderFrame(_visiblityManager, _mainCamera.Transform.Position);
-        }
-
-        private void UpdatePointLightBuffer()
-        {
-            PointLightsBuffer plb = new PointLightsBuffer();
-            plb.NumActivePointLights = Math.Min(PointLightsBuffer.MaxLights, _pointLights.Count);
-            for (int i = 0; i < plb.NumActivePointLights; i++)
-            {
-                plb[i] = _pointLights[i].GetLightInfo();
-            }
-
-            _pointLightsProvider.Data = plb;
         }
 
         public void ToggleOctreeVisualizer()
@@ -175,24 +204,66 @@ namespace Engine.Graphics
                 _octreeRenderer = null;
             }
         }
-    }
 
-    public class BoundsRenderItemEntry
-    {
-        public BoundsRenderItem BoundsRenderItem { get; private set; }
-        public Transform Transform { get; private set; }
-        public OctreeItem<RenderItem> OctreeItem { get; private set; }
-
-        public BoundsRenderItemEntry(BoundsRenderItem bri, Transform transform, OctreeItem<RenderItem> oi)
+        protected override void UpdateCore(float deltaSeconds)
         {
-            BoundsRenderItem = bri;
-            Transform = transform;
-            OctreeItem = oi;
+            float r = 0.8f;
+            float g = 0.8f;
+            float b = 0.8f;
+            Context.ClearColor = new RgbaFloat(r, g, b, 1.0f);
+
+            UpdatePointLightBuffer();
+
+            _visiblityManager.Octree.ApplyPendingMoves();
+            _renderer.RenderFrame(_visiblityManager, _mainCamera.Transform.Position);
         }
 
-        public void OnTransformChanged(Transform t)
+        private static RenderContext CreatePlatformDefaultContext(OpenTKWindow window)
         {
-            OctreeItem.Container.MarkItemAsMoved(OctreeItem, BoundsRenderItem.Bounds);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return new D3DRenderContext(window);
+            }
+            else
+            {
+                return new OpenGLRenderContext(window, false);
+            }
+        }
+
+        private void UpdatePointLightBuffer()
+        {
+            PointLightsBuffer plb = new PointLightsBuffer();
+            plb.NumActivePointLights = Math.Min(PointLightsBuffer.MaxLights, _pointLights.Count);
+            for (int i = 0; i < plb.NumActivePointLights; i++)
+            {
+                plb[i] = _pointLights[i].GetLightInfo();
+            }
+
+            _pointLightsProvider.Data = plb;
+        }
+
+        private class BoundsRenderItemEntry
+        {
+            public BoundsRenderItem BoundsRenderItem { get; private set; }
+            public Transform Transform { get; private set; }
+            public OctreeItem<RenderItem> OctreeItem { get; private set; }
+
+            public BoundsRenderItemEntry(BoundsRenderItem bri, Transform transform, OctreeItem<RenderItem> oi)
+            {
+                Debug.Assert(bri != null);
+                Debug.Assert(transform != null);
+                Debug.Assert(oi != null);
+
+                BoundsRenderItem = bri;
+                Transform = transform;
+                OctreeItem = oi;
+            }
+
+            public void OnTransformChanged(Transform t)
+            {
+                Debug.Assert(t != null);
+                OctreeItem.Container.MarkItemAsMoved(OctreeItem, BoundsRenderItem.Bounds);
+            }
         }
     }
 }
